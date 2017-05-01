0ca8bd237f41e9a26c12714fbf1a82a8a482ea62
Kedar <kedarmpster@gmail.com>
2017-05-01 15:31:14 -0400

    Incorrect commit

--- app/src/main/java/edu/buffalo/cse/cse486586/simpledynamo/SimpleDynamoProvider.java
+++ app/src/main/java/edu/buffalo/cse/cse486586/simpledynamo/SimpleDynamoProvider.java
@@ -8,8 +8,11 @@ import android.database.Cursor;
 import android.database.MatrixCursor;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Looper;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -17,24 +20,31 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.StreamCorruptedException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.locks.ReentrantLock;
 
 import static android.content.ContentValues.TAG;
 
 public class SimpleDynamoProvider extends ContentProvider {
 
+	private ReentrantLock ringLock;
 	private SharedPreferences mPrefs = null;
 	private final static String SHARED_PREF_FILENAME = "edu.buffalo.cse.cse486586.dynamo.sharedpref";
+
 	public final static String COLUMN_KEY = "key";
 	public final static String COLUMN_VALUE = "value";
 
@@ -44,6 +54,7 @@ public class SimpleDynamoProvider extends ContentProvider {
 	private ServerSocket serverSocket;
 	private String myPort;
 	private String myHash;
+	SimpleDynamoActivity ref;
 
 	@Override
 	public int delete(Uri uri, String selection, String[] selectionArgs) {
@@ -101,10 +112,12 @@ public class SimpleDynamoProvider extends ContentProvider {
 			message.put("value",value);
 			message.put("sender",myPort);
 			message.put("type","insert");
+			ringLock.lock();
 			for(String t:targets) {
 				Log.d(TAG,"Forwarding insert to:"+t+":"+ring.get(t));
 				new SendMessage().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, message.toString(), ring.get(t));
 			}
+			ringLock.unlock();
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
@@ -116,6 +129,7 @@ public class SimpleDynamoProvider extends ContentProvider {
 	}
 
 	public String[] getTargets(String key) {
+		ringLock.lock();
 		//populate 3 nodes to sen data to
 		String [] targets = new String[3];
 		for(String k:ring.keySet()) {
@@ -138,19 +152,16 @@ public class SimpleDynamoProvider extends ContentProvider {
 		//send message to coordinator and its successors (assuming no failures)
 		if(ring.higherKey(targets[0]) == null)	//target[0] is last
 			targets[1] = ring.firstKey();
-		else
-			targets[1] = ring.higherKey(targets[0]);
-
-		if(ring.higherKey(targets[1]) == null)	//target[1] is last
-			targets[2] = ring.firstKey();
-		else
 			targets[2] = ring.higherKey(targets[1]);
-
+		ringLock.unlock();
 		return targets;
 	}
 
+
 	@Override
 	public boolean onCreate() {
+		Log.e(TAG,"oncreate");
+		ringLock = new ReentrantLock();
 		TelephonyManager tel = (TelephonyManager) getContext().getSystemService(Context.TELEPHONY_SERVICE);
 		String portStr = tel.getLine1Number().substring(tel.getLine1Number().length() - 4);
 		myPort = String.valueOf(Integer.parseInt(portStr)*2);
@@ -163,6 +174,7 @@ public class SimpleDynamoProvider extends ContentProvider {
 			e.printStackTrace();
 		}
 		mPrefs = getContext().getSharedPreferences(SHARED_PREF_FILENAME, Context.MODE_PRIVATE);//multiprocess rquired?
+
 		ring = new TreeMap<String, String>();
 		ring.put(Util.genHash("5554"),"11108");
 		ring.put(Util.genHash("5556"),"11112");
@@ -174,7 +186,20 @@ public class SimpleDynamoProvider extends ContentProvider {
 		for(String k: ring.keySet()) {
 			Log.w(TAG,k+":"+ring.get(k));
 		}
-		return false;
+
+
+		//determine if this is a crash start or fresh start
+		if(mPrefs.getString("freshstart",null) == null) {
+			//fresh start. Mark it as such bu storing someting in this key
+			mPrefs.edit().putString("freshstart","yes").commit();
+		}
+		else {
+			//recovering from a crash. Remove from prefs and start synch
+//			mPrefs.edit().remove("freshstart").commit();
+			Log.d(TAG, "Starting synch after recovery");
+			synch();
+		}
+		return true;
 	}
 
 	/**
@@ -186,6 +211,8 @@ public class SimpleDynamoProvider extends ContentProvider {
 		try {
 			Map<String, String> all = (Map<String, String>) mPrefs.getAll();
 			for(String k:all.keySet()) {
+				if(k.equals("freshstart"))
+					continue;
 				JSONObject obj = new JSONObject();
 				obj.put("key",k);
 				obj.put("value",all.get(k));
@@ -197,6 +224,43 @@ public class SimpleDynamoProvider extends ContentProvider {
 		return local.toString();
 	}
 
+	public void synch() {
+		//synch from both next server.
+		// Same as query * except this time we query only our next 2 guys, combine results and store them in our sharedprefs
+		String[] targets = getTargets(myHash);
+
+		JSONObject message = new JSONObject();
+		try {
+			message.put("type","query");
+			message.put("extra","iamback");
+			message.put("sender", myPort);
+			HashMap<String,String> synched = new HashMap<String, String>();
+			for(String t:targets) {
+				String result = new SendMessage().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,message.toString(),ring.get(t)).get();
+				if(result!=null) {
+					//add contents of result into cursor
+					JSONArray keyValus = new JSONArray(result);
+					Log.d(TAG,"Synch: Received "+keyValus.length()+" entries from " + ring.get(t));
+					for(int i=0;i<keyValus.length();i++) {
+						JSONObject obj = keyValus.getJSONObject(i);
+						if(synched.get(obj.getString("key"))!=null && !synched.get(obj.getString("key")).equals(obj.getString("value")) ) {
+							Log.w(TAG,"Different values for key:" + obj.getString("key"));
+						}
+						//TODO: Compare versions somehow. synched is unused right now..
+						storeLocally(obj.getString("key"),obj.getString("value"));
+					}
+				}
+			}
+			//combine result
+		} catch (JSONException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+	}
+
 	@Override
 	public Cursor query(Uri uri, String[] projection, String selection,
 						String[] selectionArgs, String sortOrder) {
@@ -206,6 +270,8 @@ public class SimpleDynamoProvider extends ContentProvider {
 			//return everything in this node
 			Map<String, String> all = (Map<String, String>) mPrefs.getAll();
 			for(String k:all.keySet()) {
+				if(k.equals("freshstart"))
+					continue;
 				cursor.addRow(new Object[]{k,all.get(k)});
 			}
 		} else if(selection.equals("*")) {
@@ -214,17 +280,20 @@ public class SimpleDynamoProvider extends ContentProvider {
 			try {
 				message.put("type","query");
 				message.put("sender", myPort);
-
+				ringLock.lock();
 				for(String t:ring.keySet()) {
 					String result = new SendMessage().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,message.toString(),ring.get(t)).get();
-					//add contents of result into cursor
-					Log.d(TAG,"Received "+result.length()+" entries from " + ring.get(t));
-					JSONArray keyValus = new JSONArray(result);
-					for(int i=0;i<keyValus.length();i++) {
-						JSONObject kv = keyValus.getJSONObject(i);
-						cursor.addRow(new Object[]{kv.get("key"),kv.get("value")});
+					if(result!=null) {
+						//add contents of result into cursor
+						Log.d(TAG,"Received "+result.length()+" entries from " + ring.get(t));
+						JSONArray keyValus = new JSONArray(result);
+						for(int i=0;i<keyValus.length();i++) {
+							JSONObject kv = keyValus.getJSONObject(i);
+							cursor.addRow(new Object[]{kv.get("key"),kv.get("value")});
+						}
 					}
 				}
+				ringLock.unlock();
 				//combine result
 			} catch (JSONException e) {
 				e.printStackTrace();
@@ -248,6 +317,7 @@ public class SimpleDynamoProvider extends ContentProvider {
 					message.put("sender",myPort);
 					message.put("type","singlequery");
 					String latestReply = null;
+					ringLock.lock();
 					for(String t:targets) {
 						Log.d(TAG,"Forwarding query to:"+t+":"+ring.get(t));
 						String reply = new SendMessage().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, message.toString(), ring.get(t)).get();
@@ -256,6 +326,7 @@ public class SimpleDynamoProvider extends ContentProvider {
 							latestReply = reply;	//value will be available in "content" key directly
 						}
 					}
+					ringLock.unlock();
 					if(latestReply==null) {
 						Log.w(TAG,"Single query failed..none of the 3 targets responded with a value!");
 					}
@@ -279,6 +350,49 @@ public class SimpleDynamoProvider extends ContentProvider {
 		mPrefs.edit().putString(key,value).apply();
 	}
 
+	//Only for thread safety do we bother running this on the main thread
+	private void removeFromRing(final String avdPort) {
+		Handler mainHandler = new Handler(Looper.getMainLooper());
+		Runnable myRunnable = new Runnable() {
+			@Override
+			public void run() {
+				String correspondingKey = null;
+				for(String k:ring.keySet()) {
+					if(ring.get(k).equals(avdPort)) {
+						correspondingKey = k;
+						break;
+					}
+				}
+				if(correspondingKey!=null) {
+					ringLock.lock();
+					ring.remove(correspondingKey);
+					ringLock.unlock();
+					Log.w(TAG, avdPort + " removed from ring");
+					Toast.makeText(getContext(), avdPort + " removed from ring", Toast.LENGTH_SHORT).show();
+				}
+				else {
+					Log.e(TAG, "Key not found in ring");
+				}
+			}
+		};
+		mainHandler.post(myRunnable);
+	}
+
+	private void addToRing(final String avdPort) {
+		Handler mainHandler = new Handler(Looper.getMainLooper());
+		Runnable myRunnable = new Runnable() {
+			@Override
+			public void run() {
+				int port = Integer.parseInt(avdPort)/2;	//55xx number
+				ringLock.lock();
+				ring.put(Util.genHash(String.valueOf(port)),avdPort);
+				ringLock.unlock();
+				Log.w(TAG, avdPort + " added to  ring");
+				Toast.makeText(getContext(), avdPort + " added to ring", Toast.LENGTH_SHORT).show();
+			}
+		};
+		mainHandler.post(myRunnable);
+	}
 	private class SendMessage extends AsyncTask<String,Void,String> {
 
 		@Override
@@ -289,6 +403,7 @@ public class SimpleDynamoProvider extends ContentProvider {
 			try {
 				Socket socket = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 2}),
 						portNo);
+				socket.setSoTimeout(1000);
 				PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(),"UTF-8")),true);
 				BufferedReader r=new BufferedReader(new InputStreamReader(socket.getInputStream(),"UTF-8"));
 				writer.println(message);
@@ -303,15 +418,31 @@ public class SimpleDynamoProvider extends ContentProvider {
 						return obj.getString("content");
 					}
 				}
+				else {
+					Log.v(TAG,"SendMessage received null");
+					removeFromRing(strings[1]);
+				}
 				writer.close();
 				r.close();
 				socket.close();
 
-				Log.d(TAG,"Message sent");
 			} catch (UnknownHostException ex) {
-				ex.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
+				//ex.printStackTrace();
+				Log.e(TAG,strings[1] + " failed");
+				removeFromRing(strings[1]);
+			} catch(SocketTimeoutException ex) {
+				Log.e(TAG,strings[1] + " failed");
+				removeFromRing(strings[1]);
+			} catch(StreamCorruptedException ex) {
+				Log.e(TAG,strings[1] + " failed");
+				removeFromRing(strings[1]);
+			} catch (EOFException ex) {
+				Log.e(TAG,strings[1] + " failed");
+				removeFromRing(strings[1]);
+			}  catch (IOException e) {
+				//e.printStackTrace();
+				Log.e(TAG,strings[1] + " failed");
+				removeFromRing(strings[1]);
 			} catch (JSONException e) {
 				e.printStackTrace();
 			}
@@ -325,7 +456,8 @@ public class SimpleDynamoProvider extends ContentProvider {
 
 		@Override
 		protected Void doInBackground(String... strings) {
-//			String received = "";
+			String sender = "";
+
 			try {
 				while (true) {
 					Socket client = serverSocket.accept();
@@ -335,14 +467,23 @@ public class SimpleDynamoProvider extends ContentProvider {
 					String str = reader.readLine();
 					if(str!=null) {
 						JSONObject received = new JSONObject(str);
+						sender = received.getString("sender");
 						if(received.getString("type").equals("insert")) {
 							storeLocally(received.getString("key"),received.getString("value"));
+							JSONObject obj = new JSONObject();
+							obj.put("type","bullshitack");
+							writer.println(obj.toString());
 						}
 						else if(received.getString("type").equals("query")) {
+							if("iamback".equals(received.optString("extra"))) {
+								//add sender back to the ring
+								addToRing(sender);
+							}
 							JSONObject reply = new JSONObject();
 							reply.put("content",fetchLocalData());
 							reply.put("type","query_reply");
 							writer.println(reply.toString());
+
 							//maybe read again?
 
 						}
@@ -351,10 +492,16 @@ public class SimpleDynamoProvider extends ContentProvider {
 							reply.put("content",mPrefs.getString(received.getString("key"),null));
 							reply.put("type","singlequery_reply");
 							writer.println(reply.toString());
+
 						}
+
 						else {
 							Log.e(TAG,"Invalid message type!");
 						}
+					}
+					else {
+						Log.v(TAG,"AcceptMessage received null");
+						removeFromRing(sender);
 					}
 
 					writer.close();
@@ -362,8 +509,19 @@ public class SimpleDynamoProvider extends ContentProvider {
 					client.close();
 
 				}
-			}catch(IOException e){
-				e.printStackTrace();
+			}catch(SocketTimeoutException ex) {
+				Log.e(TAG,"AcceptMessages:" + sender + " failed");
+				removeFromRing(sender);
+			} catch(StreamCorruptedException ex) {
+				Log.e(TAG,"AcceptMessages:" + sender + " failed");
+				removeFromRing(sender);
+			} catch (EOFException ex) {
+				Log.e(TAG,"AcceptMessages:" + sender + " failed");
+				removeFromRing(sender);
+			} catch(IOException e){
+//				e.printStackTrace();
+				Log.e(TAG,"AcceptMessages:" + sender + " failed");
+				removeFromRing(sender);
 			} catch (JSONException e) {
 				e.printStackTrace();
 			}
